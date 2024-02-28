2c2e7c54613d7fcc729100490e6c34f7d4e0ebae
GIMME <3086660647@qq.com>
2024-02-28 18:05:06 +0800

    用户签到模块已完成

--- hm-dianping/src/main/java/com/hmdp/service/impl/UserServiceImpl.java
+++ hm-dianping/src/main/java/com/hmdp/service/impl/UserServiceImpl.java
@@ -16,6 +16,7 @@ import com.hmdp.utils.RegexUtils;
 import com.hmdp.utils.SystemConstants;
 import com.hmdp.utils.UserHolder;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.connection.BitFieldSubCommands;
 import org.springframework.data.redis.core.StringRedisTemplate;
 
 import org.springframework.stereotype.Service;
@@ -25,6 +26,7 @@ import javax.servlet.http.HttpSession;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
@@ -159,6 +161,50 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IU
         return Result.ok();
     }
 
+    @Override
+    public Result signCount() {
+        // 1、获取当前登录用户
+        Long userId = UserHolder.getUser().getId();
+        // 2、获取日期
+        LocalDateTime now = LocalDateTime.now();
+        // 3、拼接key
+        String keySuffix = now.format(DateTimeFormatter.ofPattern(":yyyy/MM"));
+        String key = USER_SIGN_KEY + userId + keySuffix;
+        // 4、获取今天是本月的第几天
+        int dayOfMonth = now.getDayOfMonth();
+
+        // 5、获取本月截至今天为止的所有的签到记录，返回一个十进制数字 bitfield key get u14 0
+        List<Long> result = stringRedisTemplate.opsForValue().bitField(
+                key,
+                BitFieldSubCommands.create()
+                        .get(BitFieldSubCommands.BitFieldType.unsigned(dayOfMonth)).valueAt(0)
+        );
+
+        if(result == null || result.isEmpty()){
+            return Result.ok(0);
+        }
+
+        Long num = result.get(0);
+        if(num == null || num == 0){
+            return Result.ok(0);
+        }
+
+        // 循环遍历
+        int count = 0;
+        while (true){
+            if((num & 1) == 0){
+                break;
+            }else{
+                count++;
+            }
+
+            num >>>= 1;     // 无符号右移移位
+        }
+
+
+        return Result.ok(count);
+    }
+
     /**
      * 通过手机号创建用户信息
      * @param phone
@@ -175,3 +221,20 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IU
         return user;
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
